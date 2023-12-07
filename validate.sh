a651440c38d9efe88435fa18691908bfb108f315
mohammedshahbaz08 <shahbaaz72870@gmail.com>
2023-12-07 22:05:12 +0530

    ....

--- validate.sh
+++ validate.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-read -p "Enter username:" name
-read -p "Enter the IP:" ip
+echo -n "Are you a student [ Yes or NO ]"
+read response
+case $response in
+      "Y"|"y"|"YES"|"Yes"|"yes")
+              echo -n "Yes, Iam a student";;
+      "N"|"n"|"NO"|"No"|"no")
+              echo -n "No, Iam not a student";;
+      *)echo -n "Invalid input";;
+esac
 
-ssh $name$ip
